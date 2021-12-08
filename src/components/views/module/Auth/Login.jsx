67d3df4f5c3a3ef97f69dd1569cbaa2e4ac25270
Nandeep2750 <barochiya.barochiya@gmail.com>
2021-12-08 15:42:05 +0530

    Change Login file to functional component

--- src/components/views/module/Auth/Login.jsx
+++ src/components/views/module/Auth/Login.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useRef, useState } from 'react'
 import SimpleReactValidator from 'simple-react-validator';
 import { Card, Form, Button, Alert } from 'react-bootstrap'
 import { toast } from 'react-toastify';
@@ -8,113 +8,103 @@ import { userActions } from '../../../redux/actions/user.actions'
 import Api from "../../../helper/Api.js";
 import { connect } from 'react-redux';
 const api = new Api();
-export class Login extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            user: {
-                email: '',
-                password: ''
-            }
-        }
+export const Login = (props) => {
 
-        this.validator = new SimpleReactValidator({
-            className: 'text-danger',
-            autoForceUpdate: this
-        });
-    }
+    const [forceUpdate, setForceUpdate] = useState(false);
+    const validator = useRef(new SimpleReactValidator({
+        className: 'text-danger',
+    }));
+
+    const [headers, setHeaders] = useState({
+        'Content-Type': 'application/json'
+    });
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    });
 
 
-    handleTextChange = (event) => {
+    const handleTextChange = (event) => {
         let name = event.target.name;
         let value = event.target.value;
 
-        let user = this.state.user;
-        user[name] = value
-
-        this.setState({ user });
+        setUser({ ...user, [name]: value });
     }
 
-    handleSubmit = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
+        if (validator.current.allValid()) {
 
-        if (this.validator.allValid()) {
             api.post("/api/login", {
-                headers: this.state.headers,
-                data: this.state.user
+                headers: headers,
+                data: user
             }).then(res => {
-                if (res.status === 200) { 
-                    this.props.login(res?.data);
-                    this.props.history.push('/dashboard');
+                if (res.status === 200) {
+                    props.login(res?.data);
+                    props.history.push('/dashboard');
                 } else {
                     toast.error(res.message);
                 }
             })
+
         } else {
-            this.validator.showMessages();
+            validator.current.showMessages();
+            setForceUpdate(!forceUpdate)
         }
     }
 
-
-    render() {
-        var { user } = this.state
-        return (
-            <div className="vh-100 d-flex align-items-center">
-                <Card style={{ width: '24rem' }} className="mx-auto">
-                    <Card.Body>
-                        <Card.Title className="text-center">Login</Card.Title>
-                        <Alert variant="info">
-                            <b> Use given credentials... </b>  <br />
-                            <b> Email: </b> eve.holt@reqres.in <br />
-                            <b> Password: </b> cityslicka <br />
-                        </Alert>
-                        <Form onSubmit={this.handleSubmit} className="mt-3" >
-                            <Form.Group className="mb-3">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control
-                                    type="email"
-                                    name="email"
-                                    placeholder="name@example.com"
-                                    value={user.email}
-                                    onChange={this.handleTextChange}
-                                    onBlur={() => this.validator.showMessageFor('email')}
-                                />
-                                {this.validator.message('email', user.email, 'required|email')}
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    name="password"
-                                    placeholder="Password"
-                                    value={user.password}
-                                    onChange={this.handleTextChange}
-                                    onBlur={() => this.validator.showMessageFor('password')}
-
-                                />
-                                {this.validator.message('password', user.password, 'required')}
-                            </Form.Group>
-                            <Button variant="primary" type="submit" className="d-flex mx-auto">
-                                Login
-                            </Button>
-                        </Form>
-                    </Card.Body>
-                </Card>
-            </div>
-        )
-    }
+    return (
+        <div className="vh-100 d-flex align-items-center">
+            <Card style={{ width: '24rem' }} className="mx-auto">
+                <Card.Body>
+                    <Card.Title className="text-center">Login</Card.Title>
+                    <Alert variant="info">
+                        <b> Use given credentials... </b>  <br />
+                        <b> Email: </b> eve.holt@reqres.in <br />
+                        <b> Password: </b> cityslicka <br />
+                    </Alert>
+                    <Form onSubmit={handleSubmit} className="mt-3" >
+                        <Form.Group className="mb-3">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type="email"
+                                name="email"
+                                placeholder="name@example.com"
+                                value={user.email}
+                                onChange={handleTextChange}
+                                onBlur={() => validator.current.showMessageFor('email')}
+                            />
+                            {validator.current.message('email', user.email, 'required|email')}
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                value={user.password}
+                                onChange={handleTextChange}
+                                onBlur={() => validator.current.showMessageFor('password')}
+                            />
+                            {validator.current.message('password', user.password, 'required')}
+                        </Form.Group>
+                        <Button variant="primary" type="submit" className="d-flex mx-auto">
+                            Login
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </div>
+    )
 }
 
 const mapStateToProps = (state) => ({
-    
+
 })
 
 const mapDispatchToProps = {
     login: userActions.login
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
